77c3c4becb222f9b55169e7408a1d9771e3f985d
tawan230666 <tawantippkun200501@gmail.com>
2025-08-13 14:30:00 +0700

    push

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -31,7 +31,7 @@ function Header() {
       <div className="container">
         <div className="header-content">
           <div className="logo">
-            <h2>Your Name</h2>
+            <h2>Tawan Tippkhun</h2>
           </div>
 
           <nav className={`nav ${isMenuOpen ? 'nav-open' : ''}`}>
