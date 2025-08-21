84375579e74bc61b35cfb8c5ed26173a58135a47
tawan230666 <tawantippkun200501@gmail.com>
2025-08-21 09:22:57 +0700

    fix: update package.json

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',   // ✅ ต้องเป็น '/' สำหรับ Netlify
+  base: '/', // สำหรับ Vercel/Netlify/Firebase ต้องเป็น '/'
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
+  }
 })
